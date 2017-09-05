4b33b7bd58d67c7b8ae6eced5ad03b111bad71f8
Bjorn Cooley <bjorncooley@gmail.com>
2017-09-05 12:06:50 -0700

    add save_user in db service

--- src/services/database_service.py
+++ src/services/database_service.py
@@ -1,3 +1,5 @@
+import sqlalchemy
+
 from config.db.database_config import DatabaseConfig
 
 
@@ -6,3 +8,12 @@ class DatabaseService:
     def __init__(self):
         db_config = DatabaseConfig()
         self.conn = db_config.engine.connect()
+        self.users = db_config.users
+
+
+    def save_user(self, username, password):
+        i = self.users.insert().values(
+            username=username,
+            password=password,
+        )
+        self.conn.execute(i)
