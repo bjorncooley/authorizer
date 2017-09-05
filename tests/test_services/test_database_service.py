9972af7801999687397374c3c50e8723882116de
Bjorn Cooley <bjorncooley@gmail.com>
2017-09-05 16:47:51 -0700

    test save_user can save first and last name'

--- tests/test_services/test_database_service.py
+++ tests/test_services/test_database_service.py
@@ -19,8 +19,8 @@ class TestDatabaseService(BaseTest):
         self.db = DatabaseService()
 
         self.conn = psycopg2.connect(
-        "dbname={0} user={1} password={2} host={3} port={4}".format(
-        LOCAL_DBNAME, LOCAL_DBUSER, LOCAL_DBPASS, LOCAL_DBHOST, DBPORT
+            "dbname={0} user={1} password={2} host={3} port={4}".format(
+            LOCAL_DBNAME, LOCAL_DBUSER, LOCAL_DBPASS, LOCAL_DBHOST, DBPORT
         ))
 
 
@@ -39,7 +39,6 @@ class TestDatabaseService(BaseTest):
     def test_database_service_can_save_user(self):
         username = 'testuser'
         password = 'testpass'
-
         self.db.save_user(
             username=username,
             password=password,
@@ -56,7 +55,6 @@ class TestDatabaseService(BaseTest):
     def test_database_service_saves_user_with_correct_username(self):
         username = 'testuser'
         password = 'testpass'
-
         self.db.save_user(
             username=username,
             password=password,
@@ -73,7 +71,6 @@ class TestDatabaseService(BaseTest):
     def test_database_service_saves_hashed_password(self):
         username = 'testuser'
         password = 'testpass'
-
         self.db.save_user(
             username=username,
             password=password,
@@ -90,7 +87,6 @@ class TestDatabaseService(BaseTest):
     def test_database_services_saves_correct_password(self):
         username = 'testuser'
         password = 'testpass'
-
         self.db.save_user(
             username=username,
             password=password,
@@ -101,4 +97,25 @@ class TestDatabaseService(BaseTest):
         curr.execute(query)
         results = curr.fetchall()
         self.assertTrue(check_password_hash(results[0][0], password))
+        curr.close()
+
+
+    def test_database_service_saves_first_and_last_name(self):
+        username = 'testuser'
+        password = 'testpass'
+        first_name = 'First'
+        last_name = 'Last'
+        self.db.save_user(
+            username=username,
+            password=password,
+            first_name=first_name,
+            last_name=last_name,
+        )
+
+        query = "SELECT first_name, last_name FROM users"
+        curr = self.conn.cursor()
+        curr.execute(query)
+        results = curr.fetchall()
+        self.assertTrue(check_password_hash(results[0][0], first_name))
+        self.assertTrue(check_password_hash(results[0][1], last_name))
         curr.close()
