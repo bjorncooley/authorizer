6d9897f90b4f70faf4ad6112249df25c7c97c87c
Bjorn Cooley <bjorncooley@gmail.com>
2017-09-05 09:57:24 -0700

    spacing

--- tests/test_api/test_api.py
+++ tests/test_api/test_api.py
@@ -7,12 +7,15 @@ class TestAPI(BaseTest):
         super(TestAPI, self).setUp()
         self.app = api.app.test_client()
 
+
     def tearDown(self):
         super(TestAPI, self).tearDown()
+
 
     def test_health_check_returns_200(self):
         response = self.app.get("/api/v1/health-check")
         self.assertEqual(200, response.status_code)
+
 
     def test_create_user_returns_200(self):
         response = self.app.post("/api/v1/create-user")
