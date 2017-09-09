1534f63855661926bbbc1918f4b3a2e7d29a9214
Bjorn Cooley <bjorncooley@gmail.com>
2017-09-09 16:17:25 -0700

    test reset password returns 200

---
+++ tests/test_api/test_reset_password.py
@@ -0,0 +1,20 @@
+
+from api import api
+from tests.base import BaseTest
+
+
+class TestResetPassword(BaseTest):
+
+    def setUp(self):
+        super(TestResetPassword, self).setUp()
+        self.app = api.app.test_client()
+
+
+
+    def tearDown(self):
+        super(TestResetPassword, self).tearDown()
+
+
+    def test_reset_password_returns_200(self):
+        result = self.app.post("/api/v1/reset-password")
+        self.assertEqual(result.status_code, 200)
