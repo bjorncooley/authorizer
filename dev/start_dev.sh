02aac92d9d18e533fd4e10f7eea7338f126bd53c
Bjorn Cooley <bjorncooley@gmail.com>
2017-11-10 12:32:37 -0800

    dev script

--- dev/start_dev.sh
+++ dev/start_dev.sh
@@ -4,4 +4,4 @@ export VIRTUALENVWRAPPER_PYTHON="/Library/Frameworks/Python.framework/Versions/3
 source /Library/Frameworks/Python.framework/Versions/3.6/bin/virtualenvwrapper.sh
 workon authorizer
 export FLASK_APP=src/api/api.py
-flask run
+flask run -p 4000
