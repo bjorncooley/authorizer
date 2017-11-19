14a61c2d10283e6523c9c6fa3e568e23a79bcde7
Bjorn Cooley <bjorncooley@gmail.com>
2017-11-18 21:21:04 -0800

    update dev start

--- dev/start_dev.sh
+++ dev/start_dev.sh
@@ -3,5 +3,5 @@ export WORKON_HOME="~/.virtualenvs"
 export VIRTUALENVWRAPPER_PYTHON='/usr/local/bin/python3'
 source /usr/local/bin/virtualenvwrapper.sh
 workon authorizer
-export FLASK_APP=src/api/api.py
+export FLASK_APP=api/api.py
 flask run -p 4000
