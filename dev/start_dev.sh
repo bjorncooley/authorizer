749add62cb81ce746f712658bd2c201bf8e97d47
Bjorn Cooley <bjorncooley@gmail.com>
2017-11-28 23:33:13 -0800

    update dev start

--- dev/start_dev.sh
+++ dev/start_dev.sh
@@ -4,5 +4,10 @@ export WORKON_HOME="~/.virtualenvs"
 export VIRTUALENVWRAPPER_PYTHON='/usr/local/bin/python3'
 source /usr/local/bin/virtualenvwrapper.sh
 workon authorizer
+
+export DBUSER=local_admin
+export DBPASS=default
+export DBNAME=missionu_main_dev
+export DBHOST=localhost
 export FLASK_APP=api/api.py
 flask run -p 4000
