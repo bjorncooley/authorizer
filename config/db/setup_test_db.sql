bfb7a3c92b4974bf209360b146d831b4b43f33d8
Bjorn Cooley <bjorncooley@gmail.com>
2018-03-15 11:08:44 -0700

    create superuser

--- config/db/setup_test_db.sql
+++ config/db/setup_test_db.sql
@@ -1,4 +1,2 @@
 CREATE DATABASE authorizer_test;
-CREATE ROLE authorizer_admin WITH LOGIN PASSWORD 'default';
-CREATE SCHEMA IF NOT EXISTS authorizer_schema AUTHORIZATION authorizer_admin;
-GRANT ALL PRIVILEGES ON SCHEMA authorizer_schema to authorizer_admin;
+CREATE ROLE authorizer_admin WITH SUPERUSER LOGIN PASSWORD 'default';
